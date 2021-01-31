da73bedbb0c61e410ba371fc1ca2340569c1d5de
Jaril Valenciano <jarilvalenciano@gmail.com>
2021-01-30 23:32:30 -0500

    Add replit version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,304 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useRef } from "react";
+import classnames from "classnames";
+import {
+  ChevronLg,
+  Share,
+  PlayLg,
+  Reply,
+  X,
+  Pencil,
+  ClickLg,
+  PlusCircle,
+  ChevronRight,
+  ChevronDown,
+  Folder,
+  Document,
+} from "./Icons";
 
 function App() {
+  const [devToolsSelected, setDevToolsSelected] = useState(false);
+  const toggleSelected = () => setDevToolsSelected(!devToolsSelected);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="bg-gray-200 w-screen h-screen flex flex-col text-gray-800">
+      <header className="bg-white w-full h-16 flex items-center justify-between p-2">
+        <div className="flex space-x-4 items-center">
+          <button className="w-8 h-8 p-1 text-gray-500 rounded-sm hover:text-blue-500">
+            <ChevronLg />
+          </button>
+          <div>My Recording</div>
+        </div>
+        <div className="flex space-x-6 items-center">
+          <button className="bg-gray-100 hover:bg-gray-200 duration-300 py-1.5 px-2 rounded-sm flex flex-row items-center space-x-2 text-sm">
+            <div className="w-4 h-4">
+              <Share />
+            </div>
+            <div>Share</div>
+          </button>
+          <Toggle toggleSelected={toggleSelected} />
+          <div className="rounded-full h-8 w-8 bg-blue-400" />
+        </div>
       </header>
+      {devToolsSelected ? <DevTools /> : <Viewer />}
+    </div>
+  );
+}
+
+function Viewer() {
+  return (
+    <main className="flex flex-col flex-grow space-y-2 p-2">
+      <section className="flex flex-row flex-grow space-x-2 ">
+        <div className="bg-gray-900 flex-grow rounded-md" />
+        <Transcript />
+      </section>
+      <Timeline />
+    </main>
+  );
+}
+
+function DevTools() {
+  return (
+    <main className="flex flex-col flex-grow space-y-2 p-2">
+      <section className="flex flex-row flex-grow space-x-2 ">
+        <Sources />
+        <div className="bg-white flex flex-col flex-grow rounded-md">
+          <header className="bg-gray-200 flex flex-row h-7 text-sm rounded-t-md">
+            <div className="text-center rounded-t-md shadow px-6 py-0.5 flex flex-row items-end bg-white">
+              <span>src.js</span>
+            </div>
+            <div className="text-center rounded-t-md shadow px-6 py-0.5 flex flex-row items-end bg-gray-100">
+              <span>app.js</span>
+            </div>
+          </header>
+          <div className="flex-grow bg-white rounded-b-md"></div>
+        </div>
+        {/* <Transcript /> */}
+        <div className="w-1/3 rounded-md flex flex-col space-y-2">
+          <div className="bg-gray-900 h-1/2 flex-grow rounded-md" />
+          <div className="bg-white h-1/2 rounded-md flex flex-col">
+            <header className="bg-gray-200 flex flex-row h-7 text-sm rounded-t-md">
+              <div className="text-center rounded-t-md shadow px-6 py-0.5 flex flex-row items-end bg-white">
+                <span>Console</span>
+              </div>
+              <div className="text-center rounded-t-md shadow px-6 py-0.5 flex flex-row items-end bg-gray-100">
+                <span>Elements</span>
+              </div>
+              <div className="text-center rounded-t-md shadow px-6 py-0.5 flex flex-row items-end bg-gray-100">
+                <span>Transcript</span>
+              </div>
+            </header>
+            <div className="flex-grow bg-white rounded-b-md"></div>
+          </div>
+        </div>
+      </section>
+      <Timeline />
+    </main>
+  );
+}
+
+function List({ header, children }) {
+  return (
+    <div className="bg-white w-96 rounded-md p-4 flex flex-col space-y-6">
+      <h2 className="font-medium">{header}</h2>
+      <div className="overflow-auto flex-grow">
+        <div className="flex flex-col space-y-6 h-0">{children}</div>
+      </div>
+    </div>
+  );
+}
+
+function Transcript() {
+  return (
+    <List header="Transcript">
+      <div className="flex flex-row justify-between hover:bg-gray-100 p-2 rounded-sm duration-300">
+        <div className="flex flex-row items-center space-x-4">
+          <div className="w-8 h-8 p-1 text-gray-500">
+            <ClickLg />
+          </div>
+          <span>Mouse Click</span>
+        </div>
+        <div>
+          <span className="rounded-md font-mono bg-gray-100 text-gray-500 text-xs p-1">
+            00:12
+          </span>
+        </div>
+      </div>
+      <Comment
+        pointTime={"00:25"}
+        user={"Jaril"}
+        time={"1:04pm"}
+        color={"blue"}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis
+      </Comment>
+      <Comment
+        pointTime={"00:42"}
+        user={"Jason"}
+        time={"3:45pm"}
+        color={"green"}
+        reply
+      >
+        Sed ut perspiciatis unde omnis iste natus error sit
+      </Comment>
+      <Comment
+        pointTime={"00:42"}
+        user={"Jaril"}
+        time={"9:29pm"}
+        color={"blue"}
+        reply
+      >
+        Incididunt ut labore et dolore magna
+      </Comment>
+      <Comment
+        pointTime={"00:52"}
+        user={"Jaril"}
+        time={"1:04pm"}
+        color={"blue"}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis
+      </Comment>
+    </List>
+  );
+}
+
+function Sources() {
+  return (
+    <List header="Sources">
+      <div className="text-sm">
+        <div className="flex flex-row items-center p-1.5 hover:bg-gray-100 rounded-sm">
+          <div className="text-gray-500 flex flex-row">
+            <div className="h-5 w-5">
+              <ChevronDown />
+            </div>
+            <div className="h-5 w-5">
+              <Folder />
+            </div>
+          </div>
+          <span className="ml-2">public</span>
+        </div>
+        <div className="flex flex-row items-center p-1.5 hover:bg-gray-100 rounded-sm">
+          <div className="text-gray-500 flex flex-row ml-10">
+            <div className="h-5 w-5">
+              <Document />
+            </div>
+          </div>
+          <span className="ml-2">app.js</span>
+        </div>
+        <div className="flex flex-row items-center p-1.5 hover:bg-gray-100 rounded-sm">
+          <div className="text-gray-500 flex flex-row">
+            <div className="h-5 w-5">
+              <ChevronRight />
+            </div>
+            <div className="h-5 w-5">
+              <Folder />
+            </div>
+          </div>
+          <span className="ml-2">private</span>
+        </div>
+      </div>
+    </List>
+  );
+}
+
+function Timeline() {
+  return (
+    <section className="h-16 w-full bg-white flex items-center justify-between p-2">
+      <button className="w-8 h-8 p-1 rounded-sm hover:text-blue-500">
+        <PlayLg />
+      </button>
+      <div className="flex-grow h-1 mx-4 text-gray-600 bg-gray-300 rounded-full" />
+      <button className="bg-gray-100 hover:bg-gray-200 duration-300 py-1.5 px-2 rounded-sm flex flex-row items-center space-x-2 text-sm">
+        <div className="w-4 h-4">
+          <PlusCircle />
+        </div>
+        <span>Add Comment</span>
+      </button>
+    </section>
+  );
+}
+
+function Comment({ children, user, time, pointTime, color, reply }) {
+  const [hovered, setHovered] = useState(false);
+  const commentNode = useRef(null);
+
+  return (
+    <div
+      className="group flex flex-row space-x-4 hover:bg-gray-100 p-2 rounded-sm duration-300"
+      ref={commentNode}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
+      <div className="flex flex-row items-center h-8 space-x-2">
+        {reply ? (
+          <div className="w-4 h-4 transform rotate-180 text-gray-400">
+            <Reply />
+          </div>
+        ) : null}
+        <div
+          className={`rounded-full flex flex-shrink-0 h-8 w-8 bg-${color}-400`}
+        />
+      </div>
+      <div className="flex flex-col flex-grow space-y-1">
+        <div className="flex flex-row justify-between items-center">
+          <div className="space-x-2">
+            <span>{user}</span>
+            <span className="text-xs text-gray-600">{time}</span>
+          </div>
+          {hovered ? (
+            <div className="flex flex-row space-x-2 transform">
+              <button className="h-5 w-5 hover:text-blue-500">
+                <Reply />
+              </button>
+              <button className="h-5 w-5 hover:text-blue-500">
+                <Pencil />
+              </button>
+              <button className="h-5 w-5 hover:text-blue-500">
+                <X />
+              </button>
+            </div>
+          ) : (
+            <span className="rounded-md font-mono bg-gray-100 text-gray-500 text-xs p-1">
+              {pointTime}
+            </span>
+          )}
+        </div>
+        <div className="text-gray-600 space-x-2 text-sm">{children}</div>
+      </div>
+    </div>
+  );
+}
+
+function Toggle({ toggleSelected }) {
+  const [isOn, setIsOn] = useState(false);
+
+  return (
+    <div className="flex items-center space-x-2">
+      <span>{isOn ? "DevTools" : "Viewer"}</span>
+      <button
+        type="button"
+        aria-pressed="false"
+        className={classnames(
+          "bg-gray-200 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none",
+          { "bg-indigo-400": isOn, "bg-blue-400": !isOn }
+        )}
+        onClick={() => {
+          setIsOn(!isOn);
+          toggleSelected();
+        }}
+      >
+        <span
+          aria-hidden="true"
+          className={classnames(
+            "pointer-events-none translate-x-0 inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200",
+            { "translate-x-5": isOn, "translate-x-0": !isOn }
+          )}
+        ></span>
+      </button>
     </div>
   );
 }
---
+++ src/Icons.js
@@ -0,0 +1,227 @@
+export function ChevronLg() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M15 19l-7-7 7-7"
+      />
+    </svg>
+  );
+}
+
+export function ShareLg() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
+      />
+    </svg>
+  );
+}
+
+export function Share() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
+    </svg>
+  );
+}
+
+export function PlayLg() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+      />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+}
+
+export function PauseLg() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+}
+
+export function Reply() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export function X() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export function Pencil() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+    </svg>
+  );
+}
+
+export function ClickLg() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
+      />
+    </svg>
+  );
+}
+
+export function PlusCircle() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export function ChevronRight() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export function ChevronDown() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export function Folder() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" />
+    </svg>
+  );
+}
+
+export function Document() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
